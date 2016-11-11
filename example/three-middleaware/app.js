53a24987b76fe9be88f3997659624b95df6630fd
lzhengms <lzhengms@gmail.com>
2016-11-11 17:36:25 +0800

    modify

--- example/three-middleaware/app.js
+++ example/three-middleaware/app.js
@@ -3,6 +3,7 @@
 const Koa = require('koa')
 const app = new Koa()
 const co = require('co')
+const convert = require('koa-convert')
 
 // async function
 app.use(async (ctx, next) => {
@@ -22,9 +23,16 @@ app.use(co.wrap(function* (ctx, next){
 })
 );
 
+// generator function, 把koa1的中间件，一个参数；转成koa2的中间件
+app.use(convert(function* (next){
+     console.log('generator function2')
+     yield next
+})
+);
+
 // common function
 app.use(ctx => {
-  console.log('common function')
+  console.log('common function2')
   ctx.body = 'three function'
 });
 
